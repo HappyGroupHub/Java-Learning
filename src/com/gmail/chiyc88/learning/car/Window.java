565fceba1996add14e38a715e5fe7987ade5833d
LD <chiyc88@gmail.com>
2021-06-01 15:18:30 +0800

    Added Window.java

---
+++ src/com/gmail/chiyc88/learning/car/Window.java
@@ -0,0 +1,12 @@
+package com.gmail.chiyc88.learning.car;
+
+import javax.swing.*;
+
+public class Window {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(1024, 768);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+}
