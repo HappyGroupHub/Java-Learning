2aea6d51ff41bd5d3fd6a6281dcc5d6a27237ee0
LD <chiyc88@gmail.com>
2021-11-16 14:04:23 +0800

    Update - Z score homework for friends

--- src/com/gmail/chiyc88/learning/Main.java
+++ src/com/gmail/chiyc88/learning/Main.java
@@ -1,10 +1,32 @@
 package com.gmail.chiyc88.learning;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int socialDistance = 2;
-        while (socialDistance > 1.5) {
-            System.out.println("bruh");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter 5 numbers: ");
+        int[] input = new int[5];
+        for (int i = 0; i <=4; i++) {
+            input[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i<= 4; i++) {
+            sum = sum + input[i];
+        }
+        int avg = sum / input.length;
+
+        double[] sbCal = new double[5];
+        double sb = 0;
+        for (int i = 0; i <= 4; i++) {
+            sbCal[i] = Math.pow(input[i] - avg, 2) / 5;
+            sb = sb + sbCal[i];
+        }
+        sb = Math.sqrt(sb);
+        double[] zScore = new double[5];
+        for (int i = 0; i <= 4; i++) {
+            zScore[i] = (input[i] - avg) / sb;
+            System.out.print(zScore[i] + ", ");
         }
     }
 }
